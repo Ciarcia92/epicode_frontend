6b93943b735a439249841240df34df90d53cee54
Lorenzo Ciarcianelli <ciarcia92@hotmail.it>
2022-10-10 12:27:36 +0200

    add last sentence

--- mese1/week3/day1/assets/Javascript/naming.js
+++ mese1/week3/day1/assets/Javascript/naming.js
@@ -11,8 +11,9 @@ console.log($pet);
 var _pet = 'leone';
 console.log(_pet);
 var _pet2 = 'pantera';
-document.write(_pet2);
 
 document.getElementById('petPrimo').innerHTML='<span style= "color:red">' + petPreferito + '</span>';
 document.getElementById('primo').innerHTML='HTML'
+
+document.write('la ' + _pet2 + ' è l\'animale piu veloce del regno animale');
 //var 12 = 'numero';
