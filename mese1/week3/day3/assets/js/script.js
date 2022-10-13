cc6a7e96b7075cabccebcdd44194961eeac78f04
Lorenzo Ciarcianelli <ciarcia92@hotmail.it>
2022-10-13 11:21:39 +0200

    add array exercises

--- mese1/week3/day3/assets/js/script.js
+++ mese1/week3/day3/assets/js/script.js
@@ -2,7 +2,7 @@
 
 function stampa (apples, oranges) {
     let succo = `Succo con ${apples} mele e ${oranges} arance`;
-    return succo
+    return succo;
  }
  document.getElementById('corretta').innerHTML= stampa(4, 5);
  document.getElementById('sbagliata').innerHTML= stampa(6);
@@ -37,25 +37,34 @@ calcolaAnno('Maria', 24, 2);
 
 /* punto 4 */
 
+const tortaSlice = (frutto) => {
+    const fette = frutto * 3;
+    return fette;
+}
 
 function cake(mele , arance){
-    function slices(){
-        document.getElementById('torta').innerHTML = `Torta con ${mele} fette di mele e ${arance} fette di arancia.`;
-    }
-    slices();
+    const fetteMela = tortaSlice(mele);
+    const fetteArancia = tortaSlice(arance);
+    document.getElementById("torta").innerHTML = `Torta con ${fetteMela} fette di mela e ${fetteArancia} fette di arancia.`;
+    return true;
 }
-cake(9,15);
+cake(3,5);
 
 /* punto 5 */
 
 const calcola = document.getElementById('calcola');
 
 calcola.addEventListener('click', function(){
-    const cibo = Number(document.getElementById('cibo').value);
-    const detersivo = Number(document.getElementById('detersivi').value);
-    const abito = Number(document.getElementById('abiti').value);
-    const spesa = cibo + detersivo + abito;
-    document.getElementById('totale').innerText = `Il totale spesa è ${spesa} &euro;`;
-    return spesa;
+    let cibo = Number(document.getElementById('cibo').value);
+    let detersivo = Number(document.getElementById('detersivi').value);
+    let abito = Number(document.getElementById('abiti').value);
+    let spesa = cibo + detersivo + abito;
+
+    document.getElementById('totale').innerHTML = `Il totale spesa è ${spesa} &euro;`;
+    
+    document.getElementById('cibo').value=''
+    document.getElementById('detersivi').value=''
+    document.getElementById('abiti').value=''
+   return spesa;
 })
 
---
+++ mese1/week3/day4/assets/js/script.js
@@ -0,0 +1,94 @@
+/*  STRINGHE */
+
+var imparo = 'Sto imparando JavaScript';
+var maiusc = imparo.toUpperCase()
+document.getElementById('maiuscola').innerHTML= maiusc;
+
+var minusc = imparo.toLowerCase();
+document.getElementById('minuscola').innerHTML= minusc;
+
+var splitted = imparo.split('');
+document.getElementById('strArray').innerHTML= splitted;
+
+var char = imparo.charAt(2);
+var char1 = imparo.charAt(1);
+var char2 = imparo.charAt(14);
+var char3 = imparo.charAt(18);
+var conc = char.concat(char1, char2, char3);
+document.getElementById('estraiCaratteri').innerHTML= conc;
+
+var slice = imparo.slice(13, 15);
+
+var conc2 = imparo.concat(slice, char3);
+document.getElementById('concatena').innerHTML= conc2;
+
+var slice5 = imparo.slice(5,9);
+document.getElementById('estraiStringa').innerHTML= slice5;
+
+/* ARRAY */
+
+const persone = ['Giovanni', 'Carla', 'Pietro', 'Mirtilla'];
+
+document.getElementById('array').innerHTML= persone.toString();
+
+document.getElementById('lunghezza').innerHTML= persone.length;
+
+document.getElementById('elemento').innerHTML= persone.slice(2,3);
+
+document.getElementById('ultimo').innerHTML= persone[persone.length-1];
+
+persone.splice(2,1, 'massimo');
+
+document.getElementById('modificato').innerHTML= persone.toString();
+
+/* ARRAY FUNZIONE */
+
+const newArray = [];
+
+// function calcAge (birthYear) {
+//     const today = 2022;
+//     const age = today - birthYear;
+//     const sentence = `${age} anni`;
+//     newArray.push(age);
+//     return sentence;
+// } 
+// document.getElementById('eta1').innerHTML+= calcAge(2001);
+// document.getElementById('eta2').innerHTML+= calcAge(1990);
+// document.getElementById('eta3').innerHTML+= calcAge(1987);
+// document.getElementById('eta4').innerHTML+= calcAge(2018);
+// document.getElementById('eta5').innerHTML+= calcAge(2010);
+// document.getElementById('arrayEta').innerHTML+= newArray;
+
+/* ARRAY FUNZIONE CON LOOP */
+
+const ages = [2001, 1990, 1987, 2018, 2010];
+for(let i = 0; i < ages.length; i++){
+    const today = 2022;
+    const age = today - ages[i];
+    const sentence = `${age} anni`;
+    newArray.push(age);
+    document.getElementById(`eta${i+1}`).innerHTML+= sentence;
+    document.getElementById('arrayEta').innerHTML="Array: " + newArray;
+}
+
+/* ARRAY ANIMALI */
+
+const animali = ['coniglio', 'cane', 'gatto', 'criceto'];
+
+
+document.getElementById('intero').innerHTML= animali;
+
+animali.push('leone');
+
+document.getElementById('aggiunto').innerHTML= animali.toString();
+
+animali.pop();
+
+document.getElementById('estratto').innerHTML= animali.toString();
+
+
+document.getElementById('verifica1').innerHTML= animali.includes('gatto');
+
+
+document.getElementById('verifica2').innerHTML= animali.includes('ornitorinco');
+
