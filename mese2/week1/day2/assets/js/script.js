33d94e7f37b10ba243a975f8c125a4f4c04a311a
Lorenzo Ciarcianelli <ciarcia92@hotmail.it>
2022-10-25 10:51:18 +0200

    added exercise

---
+++ mese2/week1/day2/assets/js/script.js
@@ -0,0 +1,44 @@
+
+let btn = document.getElementById('start');
+
+btn.addEventListener('click', function(){
+    let oldBudget = Number(document.getElementById('budget').value);
+
+    let startingExpense= 0;
+    let expense= 0;
+    let newBudget= oldBudget;
+    document.getElementById('starting-budget').innerHTML+= oldBudget;
+
+    for(startingExpense=0; newBudget>(oldBudget/10); startingExpense++) {
+        expense = Math.floor(Math.random() *  oldBudget/10);
+        newBudget -= expense;
+        document.getElementById('list').innerHTML+= "<li>Ammontare della spesa: €" + expense + "</li>";
+        document.getElementById('list').innerHTML += "<li>Nuovo budget aggiornato: €" + newBudget + "</li>";
+    }
+    document.getElementById('list').innerHTML += "<h2>Hai speso abbastanza</h2>";
+    document.getElementById('list').innerHTML += "<p>Totale pese effettuate: " + expense + "</p>";
+    document.getElementById('list').innerHTML += "<p>Budget rimanente: €" + newBudget + "</p>";
+
+
+
+        /*
+    }
+        var newBudget;
+        console.log('ammontare della spesa: ', expense)
+        newBudget= oldBudget-expense;
+        console.log('nuovo budget aggiornato: ', newBudget);
+    for(let i= 0; i< expenses.length; i++) {
+        let newBudget;
+            console.log('ammontare della spesa: ', expenses[i]);
+            newBudget= oldBudget-expenses[i];
+            console.log('nuovo budget aggiornato: ', newBudget);
+    }
+    */
+});
+
+
+
+
+
+
+
