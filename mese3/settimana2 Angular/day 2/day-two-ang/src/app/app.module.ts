474f9f42e26c9b05e751c7df5f4f46470eff84d9
Lorenzo Ciarcianelli <ciarcia92@hotmail.it>
2022-11-30 09:07:51 +0100

    routing angular

---
+++ mese3/settimana2 Angular/day 2/day-two-ang/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { PostAttiviComponent } from './components/post-attivi/post-attivi.component';
+import { PostInattiviComponent } from './components/post-inattivi/post-inattivi.component';
+import { HomeComponent } from './components/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    PostAttiviComponent,
+    PostInattiviComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
