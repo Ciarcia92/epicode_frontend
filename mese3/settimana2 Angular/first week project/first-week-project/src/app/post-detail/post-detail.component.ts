4a11cb0023a6daf5d479fe50556cf77ebb823ba5
Lorenzo Ciarcianelli <ciarcia92@hotmail.it>
2022-12-02 11:57:43 +0100

    added icon

--- mese3/settimana2 Angular/first week project/first-week-project/src/app/post-detail/post-detail.component.ts
+++ mese3/settimana2 Angular/first week project/first-week-project/src/app/post-detail/post-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common'
 
 @Component({
   selector: 'app-post-detail',
@@ -10,11 +11,15 @@ export class PostDetailComponent implements OnInit {
 
   post: any;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private location: Location) { }
 
   ngOnInit(): void {
     // console.log(this.route.queryParams.subscribe(params => {console.log(params);}));
     this.post = history.state.data;
+  }
+
+  back(): void {
+    this.location.back()
   }
 
 }
